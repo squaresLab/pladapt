0b53d092c738385ffda24dd7bc6d9be01c8c73a3
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-07-03 23:48:56 -0400

    process test

--- examples/dart/darteval/SASSAdaptationManager.cpp
+++ examples/dart/darteval/SASSAdaptationManager.cpp
@@ -3,16 +3,22 @@
 #include <pladapt/UtilityFunction.h>
 #include <pladapt/Configuration.h>
 #include <pladapt/ConfigurationManager.h>
-//#include <boost/process.hpp>
+#include <boost/process.hpp>
 
 using namespace std;
 
-//namespace bp = boost::process;
+namespace bp = boost::process;
 
 namespace pladapt {
 
 	TacticList SASSAdaptationManager::evaluate(const Configuration& currentConfigObj, const EnvironmentDTMCPartitioned& envDTMC,
         const UtilityFunction& utilityFunction, unsigned horizon){
+
+		std::string exec = "echo test"; 
+
+		bp::system("echo test", bp::std_out > stdout, bp::std_err >
+stderr, bp::std_in < stdin);
+
 		set<string> str { "GoTight" };
     	return str;
      }
